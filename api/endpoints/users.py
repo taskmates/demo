829a1bd2c66cc377284578fbe4d8d86376252573
srizzo <srizzo@users.noreply.github.com>
2024-06-15 19:53:01 +0000

    Fix 500 error in /users/<built-in function id> endpoint when user has no email

--- api/endpoints/users.py
+++ api/endpoints/users.py
@@ -23,6 +23,8 @@ async def get_user(user_id: int, db: Session = Depends(get_db)):
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
-    email_domain = user.email.split("@")[1]
+    user_data = {"id": user.id, "name": user.name}
+    if user.email:
+        user_data["email_domain"] = user.email.split("@")[1]
 
-    return {"id": user.id, "name": user.name, "email_domain": email_domain}
+    return user_data
