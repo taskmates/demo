ae6050fb2c23a2044701fb7593c1bfbc36ee4c94
Samuel Rizzo <samuel365@gmail.com>
2024-08-05 16:29:33 +0300

    Adiciona tratamento de erro e logging para o endpoint de usuários

--- api/endpoints/users.py
+++ api/endpoints/users.py
@@ -23,6 +23,8 @@ async def get_user(user_id: int, db: Session = Depends(get_db)):
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
-    email_domain = user.email.split("@")[1]
+    email_domain = None
+    if user.email:
+        email_domain = user.email.split("@")[1]
 
     return {"id": user.id, "name": user.name, "email_domain": email_domain}
